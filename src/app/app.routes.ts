39cc183183ce2152a2e1283d8f90fc03e0af2847
itait8 <itait8@gmail.com>
2024-06-25 20:48:03 +0300

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { ChannelComponent } from './Components/channel/channel.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'Home',
+    component: HomeComponent,
+  },
+  {
+    path: 'Channel',
+    component: ChannelComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'Home',
+  },
+];
